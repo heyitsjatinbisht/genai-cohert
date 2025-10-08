d28af3603d1e23b1f9e235d5ae3e845018bd6856
Jsbist21 <bishtjatin477@gmail.com>
2025-10-08 16:50:40 +0000

    langgraph

--- 06_rag_queue/server.py
+++ 06_rag_queue/server.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from .queue.connection import queue
 from .queue.worker import process_query
+
 
 app = FastAPI()
 
@@ -16,3 +17,13 @@ def chat(
 ):
     job = queue.enqueue(process_query, query)
     return {"status": "Job enqueued", "job_id": job.id}
+
+
+@app.get("/result/{job_id}")
+def get_result(
+    job_id: str = Path(..., decription="Job ID")
+):
+    job = queue.fetch_job(job_id=job)
+    result = job.return_value()
+
+    return {"result": result}
